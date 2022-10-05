9f00d31a888acd2ce966b40d16eea79e60c5d850
6-PK 2E <114576919+RustlingTitan36@users.noreply.github.com>
2022-10-05 10:59:04 +0200

    Create KartaPracy2.cs

---
+++ KartaPracy2.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+            */
+
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 100 && a < 1000 && a % 17 == 0)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+            */
+
+            /*
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek >= 18)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+            */
+
+            /*
+            int limit = 20;
+            int waga = int.Parse(Console.ReadLine());
+            if (waga <= limit)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+            */
+
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a > c && c > b)
+            {
+                Console.WriteLine("Tak");
+            }
+            else if (b > c && c > a)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+            */
+
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            for (p = 2; p < a; p++)
+            {
+                if ((Math.Pow(a, p) - a) % p == 0)
+                {
+                    Console.WriteLine("Tak");
+                }
+                else
+                {
+                    Console.WriteLine("Nie");
+                }
+            }
+            */
+
+            /*
+            int p = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+
+            if ((p + 3*s) >= k)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+            */
+        }
+    }
+}
