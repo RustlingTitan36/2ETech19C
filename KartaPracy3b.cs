92c6c32b3072cdb1ae48df85679bcaedf862a782
6-PK 2E <114576919+RustlingTitan36@users.noreply.github.com>
2022-10-25 19:26:55 +0200

    Create KartaPracy3b.cs

---
+++ KartaPracy3b.cs
@@ -0,0 +1,80 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    /*
+    for (int i = 1; i<=30; i++)
+    {
+      Console.WriteLine(i);
+    }
+    
+    for (int i = 1; i<=100; i+=2)
+    {
+      Console.WriteLine($"{i}^2=${i*i}");
+    }
+    
+    int x = 1000;
+    while (x<=9999)
+    {
+      if (x%379==0)
+      {
+        Console.WriteLine(x);
+      }
+      x = x + 1;
+    }
+    
+    int x = 100;
+    while (x<=999)
+    {
+      if (x%5==0)
+      {
+        Console.WriteLine(x);
+      }
+      else if (x%6==0)
+      {
+        Console.WriteLine(x);
+      }
+      else if (x%11==0)
+      {
+        Console.WriteLine(x);
+      }
+      x = x + 1;
+    }
+    
+    int x, i, suma, ilosc;
+    Console.WriteLine("Ile podasz liczb: ");
+    ilosc = int.Parse(Console.ReadLine());
+    suma = 0;
+    i = 1;
+    while (i<=ilosc)
+    {
+      Console.WriteLine($"Podaj liczbę nr {i}");
+      x = int.Parse(Console.ReadLine());
+      suma = suma + x;
+      i = i + 1;
+    }
+    Console.WriteLine($"Suma danych liczb to: {suma}");
+    
+    int k, suma;
+    Console.WriteLine("Podaj ilość liczb parzystych: ");
+    k = int.Parse(Console.ReadLine());
+    suma = 0;
+    for (int i = 0; i<k; i++)
+    {
+      suma = suma + 2;
+    }
+    Console.Write(suma);
+    
+    int k, suma;
+    Console.WriteLine("Podaj ilość liczb dwucyfrowych nieparzystych: ");
+    k = int.Parse(Console.ReadLine());
+    suma = 11;
+    for (int i = 0; i<=k; i++)
+    {
+      suma = suma + 2;
+    }
+    Console.Write(suma);
+    */
+  }
+}
