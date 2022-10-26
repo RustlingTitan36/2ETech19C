9294dcbb58e8a3b1bd458b2ba4e57b73eb237ac6
6-PK 2E <114576919+RustlingTitan36@users.noreply.github.com>
2022-10-26 10:11:37 +0200

    Update KartaPracy3a.cs

--- KartaPracy3a.cs
+++ KartaPracy3a.cs
@@ -1,48 +1,54 @@
-using System; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Program 
+namespace ConsoleApp1
 {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ZAD1
+            /*
+            int n;
+            Console.WriteLine("Podaj liczbę");
+            n = int.Parse(Console.ReadLine());
+            for (int i=0; i<=n; i++)
+            {
+              Console.Write("* - | ");
+            }
+            */
 
-  public static void Main(string[] args) 
-  {
-    // ZAD1
-    /*
-    int n;
-    Console.WriteLine("Podaj liczbę");
-    n = int.Parse(Console.ReadLine());
-    for (int i=0; i<=n; i++)
-    {
-      Console.Write("* - | ");
-    }
-    */
+            //ZAD2
+            /*
+            for (int i=1; i<11; i++)
+            {
+              for (int j=1; j<11; j++)
+              {
+                Console.Write(i*j + "\t");
+              }
+              Console.WriteLine();
+            }
+            */
 
-    //ZAD2
-    /*
-    for (int i=1; i<11; i++)
-    {
-      for (int j=1; j<11; j++)
-      {
-        Console.Write(i*j + "\t");
-      }
-      Console.WriteLine();
+
+            int n;
+            Console.WriteLine("Podaj liczbę");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                for (int l = 0; l <= i; l++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
     }
-    */
-    
-    
-    int n;
-    Console.WriteLine("Podaj liczbę");
-    n = int.Parse(Console.ReadLine());
-    for (int i=0; i<=n; i++)
-    {
-      for (int l=0; l<=i; l++)
-      {
-        Console.Write(" ");
-      }
-      for (int j=n; j>i; j--)
-      {
-        Console.Write("*");
-      }
-      Console.WriteLine();
-    }
-  }
 }
