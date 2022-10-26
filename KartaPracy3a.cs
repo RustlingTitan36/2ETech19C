96d5eb3a84585e95c0d932be10a5c7b6f7213c40
6-PK 2E <114576919+RustlingTitan36@users.noreply.github.com>
2022-10-26 12:20:27 +0200

    Update KartaPracy3a.cs

--- KartaPracy3a.cs
+++ KartaPracy3a.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,7 +34,7 @@ namespace ConsoleApp1
             }
             */
 
-
+            /*
             int n;
             Console.WriteLine("Podaj liczbę");
             n = int.Parse(Console.ReadLine());
@@ -46,6 +47,116 @@ namespace ConsoleApp1
                 for (int j = n; j > i; j--)
                 {
                     Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Podaj liczbę");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                for (int l = 0; l <= i; l++)
+                {
+                    Console.Write("#");
+                }
+                for (int j = n; j > i + 1; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                
+            }
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+
+            //*****
+            //****
+            //***
+            //**
+            //*
+
+            //    *
+            //   **
+            //  ***
+            // ****
+            //*****
+
+
+            int n;
+            Console.WriteLine("Podaj liczbę");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n + 1; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    if (i >= j) Console.Write("");
+                    else Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i <=n + 1; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (n-j >= i) Console.Write(" ");
+                    else Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    if (i == 1 || j == 1 || i == n || j == n || (i == n/2 + 1 && j == n/2 + 1)) Console.Write("*");
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    if (i == n-j+1) Console.Write("?");
+                    else if (i == j) Console.Write("*");
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    if (j == n/2+1) Console.Write("*");
+                    else if (i == n/2+1) Console.Write("-");
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            */
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    if (i + j == n/2 + 2 || i + j == 15) Console.Write("*");
+                    else Console.Write("#");
                 }
                 Console.WriteLine();
             }
