6ffaef30373de81dd137f9f38c2e127b7cb991f6
6-PK 2E <114576919+RustlingTitan36@users.noreply.github.com>
2022-12-14 11:09:07 +0100

    Update KartaObiektowa1a.cs

--- KartyObiektowe/KartaObiektowa1a.cs
+++ KartyObiektowe/KartaObiektowa1a.cs
@@ -6,67 +6,83 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp11
 {
-	class Producent {
-		public string nazwa;
-		public string nip;
-		public string getNazwa()
-		{
-			return nazwa;
-		}
-		public string getNip()
-		{
-			return nip;
-		}
-		public void setNazwa()
-		{
-			Console.WriteLine("nazwa?")
-		}
-	}
+    public class Producent
+    {
+        public string nazwa;
+        public string nip;
+        private string getNazwa()
+        {
+            return nazwa;
+        }
+        private string getNip()
+        {
+            return nip;
+        }
+        private void setNazwa(string anazwa)
+        {
+            this.nazwa = anazwa;
+        }
+    }
+    public class Material
+    {
+        private string typ;
+        private int id;
+        private double cena;
+        public void setTyp(string atyp)
+        {
+            this.typ = atyp;
+        }
+        public string getTyp()
+        {
+            return typ;
+        }
+        public string getCena()
+        {
+            return "Cena";
+        }
+        private void setId(int aid)
+        {
+            this.id = aid;
+        }
+    }
+    public class Rower
+    {
+        private Producent producent;
+        private string color;
+        private Material material;
+        public void jedz()
+        {
+            Console.WriteLine("  ");
 
-	class Material {
-		public string typ;
-		public int id;
-		public double cena;
-		public void setTyp()
-		{
-			Console.WriteLine("typ?")
-		}
-		public string getTyp()
-		{
-			return typ;
-		}
-		public string getCena()
-		{
-			return cena;
-		}
-		public void setId()
-		{
-			Console.WriteLine("id?")
-		}
-	}
-
-	class Rower {
-		public Producent producent;
-		public string color;
-		public Material material;
-		public void jedz()
-		{
-			Console.WriteLine("  ")
-		}
-		public void setColor()
-		{
-			Console.WriteLine("kolor?")
-		}
-		public string getColor()
-		{
-			return color;
-		}
-		}
-	class Program {
-		public static void Main(string[] args) {
-			Console.WriteLine("hello world");
-		}
-	}
+        }
+        public void setColor(string acolor)
+        {
+            this.color = acolor; 
+        }
+        public string getColor()
+        {
+            return color;
+        }
+        public void setMaterial(Material material)
+        {
+            this.material = material;
+        }
+        public Rower(Producent aproducent)
+        {
+            this.producent = aproducent;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Producent p1 = new Producent();
+            Material m1 = new Material();
+            Rower r1 = new Rower(p1);
+            r1.setMaterial(m1);
+            r1.setColor("Czrewony");
+        }
+    }
 }
